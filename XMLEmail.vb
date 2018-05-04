6a8592de1f006316521acc34a8f8be66588288d9
Jasmine-Ball <jasmine.ball@uon.edu.au>
2018-05-05 01:06:38 +1000

    email addresses as xml input, panel update

--- XMLEmail.vb
+++ XMLEmail.vb
@@ -6,11 +6,11 @@ Imports System.Text.RegularExpressions
 
 Public Class XMLEmail
 
-    Private Sub SendButtonClick(sender As Object, e As EventArgs) Handles btnSend.Click
+    Private Sub SendButtonClick(panel As Object, e As EventArgs) Handles BtnSend.Click
 
         Try
             ' Declared here so that the variables would be available to multiple functions	
-            Dim custAccountNumberXMLData As String = ""   
+            Dim custAccountNumberXMLData As String = ""
             Dim custFirstNameXMLData As String = ""
             Dim custLastNameXMLData As String = ""
             Dim custAddress1XMLData As String = ""
@@ -18,16 +18,17 @@ Public Class XMLEmail
             Dim custCityXMLData As String = ""
             Dim custStateXMLData As String = ""
             Dim custPostCodeXMLData As String = ""
-            Dim custCountry As String = ""
+            Dim custCountryXMLData As String = ""
+            Dim custEmailXMLData As String = ""
             Dim HTMLDataPopulated As String = ""
 
             ' Reading in template for Regex
-            Dim HTMLTemplateRead As String = My.Computer.FileSystem.ReadAllText("C:\cid.html") 'Reading in the HTML template as text
+            Dim HTMLTemplateRead As String = My.Computer.FileSystem.ReadAllText("C:\CCM\Templates\binTemplate.html") 'Reading in the HTML template as text
 
             ' Sender email credential management
-            Dim eml As String = "sender@gmail.com" 'This will hold the sender's email address
-            Dim pwd As String 'Senders password will need to be entered into the InputBox and sent over SSL as below
-            pwd = InputBox("Enter sender password") 'Prompt to sender
+            Dim eml As String = "testjunke@gmail.com" 'This will hold the sender's email address
+            Dim pwd As String = TbxPass.Text 'Senders password will need to be entered. Later sent over SSL
+
 
             ' Looping through the customers in the XML file
             Dim count As Integer = 1 ' This is used as the starting array index for the XMLParse function
@@ -42,7 +43,8 @@ Public Class XMLEmail
                 custCityXMLData = XMLParse("CustCity", count)
                 custStateXMLData = XMLParse("CustState", count)
                 custPostCodeXMLData = XMLParse("CustPostCode", count)
-                custCountry = XMLParse("CustCountry", count)
+                custCountryXMLData = XMLParse("CustCountry", count)
+                custEmailXMLData = XMLParse("CustEmail", count)
                 count += 1
 
                 ' Regex cutomer details placement via CustomerWrite function
@@ -54,7 +56,7 @@ Public Class XMLEmail
                 Dim HTMLWithCustCity As String = CustomerWrite("CustCity", custCityXMLData, HTMLWithCustAddress2)
                 Dim HTMLWithCustState As String = CustomerWrite("CustState", custStateXMLData, HTMLWithCustCity)
                 Dim HTMLWithCustPostcode As String = CustomerWrite("CustPostCode", custPostCodeXMLData, HTMLWithCustState)
-                Dim HTMLWithCustCountry As String = CustomerWrite("CustCountry", custCountry, HTMLWithCustPostcode)
+                Dim HTMLWithCustCountry As String = CustomerWrite("CustCountry", custCountryXMLData, HTMLWithCustPostcode)
                 HTMLDataPopulated = HTMLWithCustCountry
 
                 ' Creating SMTP based email objects
@@ -69,16 +71,16 @@ Public Class XMLEmail
                 Smtp.Host = "smtp.gmail.com"
 
                 ' Image embedding starts here
-                Dim logoBin = New System.Net.Mail.Attachment("C:\logo_bin.PNG") 'Reading in the logo as a jpeg
+                Dim logoBin = New System.Net.Mail.Attachment("C:\CCM\Templates\logo_bin.PNG") 'Reading in the logo as a jpeg
                 logoBin.ContentId = "logo_bin.PNG"
-                Dim sigCEO = New System.Net.Mail.Attachment("C:\sig_mfitzgerald_mc_300.jpeg") 'Reading in the CEO's Signature as a jpeg
+                Dim sigCEO = New System.Net.Mail.Attachment("C:\CCM\Templates\sig_mfitzgerald_mc_300.jpeg") 'Reading in the CEO's Signature as a jpeg
                 sigCEO.ContentId = "sig_mfitzgerald_mc_300.jpeg" 'Giving the image an identifier for use in the HTML file
 
                 ' Setting the email properties
                 mail.Body = HTMLDataPopulated 'Custom email content based on HTML Template
-                mail.From = New MailAddress("sender@gmail.com")
-                mail.To.Add("customer@gmail.com")
-                mail.Subject = "Welcome"
+                mail.From = New MailAddress("testjunke@gmail.com")
+                mail.To.Add(custEmailXMLData) 'Destination email address based on XML
+                mail.Subject = "Welcome, " & custFirstNameXMLData
                 mail.Attachments.Add(sigCEO)
                 mail.Attachments.Add(logoBin)
                 mail.IsBodyHtml = True
@@ -96,7 +98,7 @@ Public Class XMLEmail
     ' Parsing XML tags for each customer
     Function XMLParse(element, count) As String
         Dim regSelect = "./Customer[" & count & "]"
-        Dim document As XElement = XElement.Load("C:\customerList.xml")
+        Dim document As XElement = XElement.Load("C:\CCM\Input\customerList.xml")
         Dim oneElement As XElement = document.XPathSelectElement(regSelect.ToString).XPathSelectElement(element)   'Selecting the details of the customer
         Dim oneElementAsString = oneElement.ToString
         Dim oneElementAsStringRegex As Regex = New Regex("<.*?>") 'Regex filtering is being used to tidy XML tags
@@ -111,8 +113,19 @@ Public Class XMLEmail
     Function CustomerWrite(dataLoc, XMLField, HTMLTemplateRead) As String
         Dim textLocationRegex As Regex = New Regex(dataLoc)
         Dim custData As Match = textLocationRegex.Match(HTMLTemplateRead)
-        Dim HTMLWithCustomData As String = HTMLTemplateRead.Replace(custData.ToString, XMLField) 'Replacing generic custName text with actual customer name.
+        Dim HTMLWithCustomData As String = HTMLTemplateRead.Replace(custData.ToString, XMLField) 'Replacing generic placeholder text with actual customer name.
         Return HTMLWithCustomData
     End Function
 
+    Private Sub XMLEmail_Load(panel As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TbxPass_TextChanged(panel As Object, e As EventArgs) Handles TbxPass.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LblPass.Click
+
+    End Sub
 End Class
